7b8b490000a580155314714d12fb0a22425ec5e2
Prachi <35754388+prachighosh@users.noreply.github.com>
2018-02-05 08:42:56 +0530

    Add files via upload

---
+++ Prachi.py
@@ -0,0 +1,14 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> print("ABC")
+ABC
+>>> print("abc")
+abc
+>>> a=25
+>>> b=35
+>>> c=a+b
+>>> print(c)
+60
+>>> a=67
+>>> b=45
---
+++ randomfile.py
@@ -0,0 +1,16 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> import random
+>>> print(random.randint(1,100))
+76
+>>> import random
+>>> print(random.randint(1,100))
+93
+>>> import random
+>>> print(random.randint(1,100))
+85
+>>> import random
+>>> print(random.randint(1,100))
+15
+>>> 
