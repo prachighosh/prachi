6889fe937a3970fdb129e66ffddc37ba56200797
Prachi <35754388+prachighosh@users.noreply.github.com>
2018-04-11 09:03:36 +0530

    Add files via upload

---
+++ addition4.py
@@ -0,0 +1,20 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> a=1
+>>> b=2
+>>> print(a+b)
+3
+>>> a=1
+>>> b=2
+>>> print(a-b)
+-1
+>>> a=2
+>>> b=3
+>>> print(a*b)
+6
+>>> a=2
+>>> b=2
+>>> print(a/b)
+1.0
+>>> 
---
+++ name.py
@@ -0,0 +1,5 @@
+#printing my name
+print["PRACHI"]
+print("i am doing engineeering in CMRU")
+print(100+10)
+print(2%10)
