c0d02d362543c78132d66ff88a37789cc7660095
Prachi <35754388+prachighosh@users.noreply.github.com>
2018-04-11 09:19:37 +0530

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,19 @@
+def add(x,y):
+    print(x+y)
+def sub(x,y):
+    return(x-y)
+def mul(x,y):
+    return(x*y)
+def div(x,y):
+    return(x/y)
+def flo(x,y):
+    return(x//y)
+def mod(x,y):
+    return(x%y)
+add(400,500)
+sub(400,500)
+mul(400,500)
+div(400,500)
+flo(400,500)
+print(mod(400,500))
+
