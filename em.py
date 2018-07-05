51e4bcb8e5ef988a36bde5e68fc3b4b65e12a4b5
Prachi <35754388+prachighosh@users.noreply.github.com>
2018-07-05 11:49:05 +0530

    Delete em.py

--- em.py
+++
@@ -1,8 +0,0 @@
-import smtplib
-s = smtplib.SMTP('smtp.gmail.com', 587)
-s.starttls()
-s.login("prachi.17cs.cmr.edu", "poush")
-message = "hii!"
-s.sendmail("prachi.17cs@cmr.edu.in", "vibhasree49@gmail.com", message)
-s.quit()
-
